897356a49bcf5061daf8142217417668148731d4
Taras Novak <taras.novak@randomfractals.com>
2019-03-04 08:39:29 -0600

    refined and simplified map center and maxBounds config
    for proper zoom on paradise campfire region and display of all buildings affected

--- src/common/config.js
+++ src/common/config.js
@@ -1,6 +1,7 @@
 import { timeParser } from './time-utils';
 
 // map data replay start and end date
+export const startDateString = '2018-11-08 00:00:00';
 export const startDate = timeParser('November 08 2018');
 export const endDate = timeParser('November 25 2018');
 
@@ -13,11 +14,11 @@ export const mapboxAccessToken = 'apiKey';
 export const mapboxConfig = {
   container: 'map',
   style: 'mapbox://styles/mapbox/light-v9',
-  center: [-121.6219177, 39.7596061],
+  center: [-121.62, 39.70],
   zoom: 2,
   minZoom: 2,
   maxZoom: 16,
-  maxBounds: [[-121.82057757193864, 39.43823008560554], [-121.38840846343999, 39.868764602121246]]
+  maxBounds: [[-121.82, 39.60], [-121.38, 39.86]]
 };
 
 // mapd server connection string
