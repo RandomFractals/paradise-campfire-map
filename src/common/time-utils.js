2d50f4f8e11f8274af3a16149e2d68305c573a5b
Taras Novak <taras.novak@randomfractals.com>
2019-03-05 14:13:52 -0600

    utc date display and time format patch

--- src/common/time-utils.js
+++ src/common/time-utils.js
@@ -1,13 +1,13 @@
 import { scaleTime } from 'd3-scale';
-import { timeFormat } from 'd3-time-format';
+import { utcFormat } from 'd3-time-format';
 import { startDate, endDate } from './config';
 
 // https://github.com/d3/d3-time-format#locale_format
 const timeFormatString = '%Y-%m-%d 00:00:00';
 const dayFormatString = '%B %d, %A, %I %p'; // November 8, Monday, 6 AM
 
-export const timeFormatter = timeFormat(timeFormatString);
-export const dayFormatter = timeFormat(dayFormatString);
+export const timeFormatter = utcFormat(timeFormatString);
+export const dayFormatter = utcFormat(dayFormatString);
 export const dayCount = Math.round(Math.abs(endDate.getTime() - startDate.getTime())/8.64e7); // msecs in a day
 export const timeScale = scaleTime()
   .range([0, dayCount])
--- src/components/time-label.js
+++ src/components/time-label.js
@@ -6,10 +6,12 @@ let timeLabel = null;
 export function initTimeLabel() {
   timeLabel = document.querySelector('label.time-label');
   timeLabel.innerHTML = dayFormatter(startDate);
+  console.log('time-label:startDate:', startDate.toUTCString());
 }
 
 export function updateTimeLabel(date) {
-  timeLabel.innerHTML = dayFormatter(date);
+  timeLabel.innerHTML = dayFormatter(
+    new Date(date.getTime() + (date.getTimezoneOffset() * 60000))); // to UTC
 }
 
 export default timeLabel;
