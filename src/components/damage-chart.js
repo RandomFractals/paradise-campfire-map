28c6bfa9d1723700347c27716fbd2a744740ac53
Taras Novak <taras.novak@randomfractals.com>
2019-03-07 18:26:09 -0600

    added quick and dirty damage vega bar chart display

--- src/components/damage-chart.js
+++ src/components/damage-chart.js
@@ -1,13 +1,41 @@
+import {default as vegaEmbed } from 'vega-embed';
+import { getLabel } from '../common/damage-color-palette';
+
 let damageChart = null;
 
 export function initDamageChart() {
   damageChart = document.querySelector('#damage-chart');
-  damageChart.innerHTML = '<p>TODO: add damage by category bar chart</p>';
+  damageChart.innerHTML = '<p>damage by category bar chart</p>';
 }
 
 export function updateDamageChart(damageData) {
   console.log('damage-chart:updateDamageChart(): damage-data:', damageData);
-  // TODO: add vegaLite damage horizontal bar chart display
+  const sortedDamageData = damageData.sort((a, b) => b.val - a.val);
+  // damage bar chart vega spec
+  const vegaSpec = {
+    "$schema": "https://vega.github.io/schema/vega-lite/v3.json",
+    "data": {
+      "values": sortedDamageData
+    },
+    "encoding": {
+      "y": {"field": "key0", "type": "ordinal"},
+      "x": {"field": "val", "type": "quantitative"}
+    },
+    "layer": [{
+      "mark": "bar"
+    }, {
+      "mark": {
+        "type": "text",
+        "align": "left",
+        "baseline": "middle",
+        "dx": 3
+      },
+      "encoding": {
+        "text": {"field": "val", "type": "quantitative"}
+      }
+    }]
+  };
+  vegaEmbed('#damage-chart', vegaSpec);
 }
 
 export default damageChart;
