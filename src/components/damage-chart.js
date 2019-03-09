6baa8c41f5eccc887658489896a70011acce5be0
Taras Novak <taras.novak@randomfractals.com>
2019-03-08 21:01:04 -0600

    missed some ;)

--- src/components/damage-chart.js
+++ src/components/damage-chart.js
@@ -6,14 +6,14 @@ let damageChart = null;
 export function initDamageChart() {
   damageChart = document.querySelector('#damage-chart');
   damageChart.innerHTML = '<p>damage by category bar chart</p>';
-}
+};
 
 export function updateDamageChart(damageData) {
   console.log('damage-chart:updateDamageChart(): damage-data:', damageData);
-  const chartData = damageData.sort((a, b) => b.val - a.val)
-    .map(damage => {
+  const chartData = damageData.map(damage => {
       return {damage: getLabel(damage.key0), color: getColor(damage.key0), count: damage.val};
     });
+  
   // damage bar chart vega spec
   const vegaSpec = {
     "$schema": "https://vega.github.io/schema/vega-lite/v3.json",
@@ -61,7 +61,8 @@ export function updateDamageChart(damageData) {
       }
     }]
   };
+
   vegaEmbed('#damage-chart', vegaSpec);
-}
+};
 
 export default damageChart;
