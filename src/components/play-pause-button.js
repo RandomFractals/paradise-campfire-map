4f4ed4f3e1728214f68fa830fd35264bd4b0e5d3
Taras Novak <taras.novak@randomfractals.com>
2019-03-10 18:01:38 -0500

    simplified playback timer setup

--- src/components/play-pause-button.js
+++ src/components/play-pause-button.js
@@ -1,15 +1,11 @@
 import { interval } from 'd3-timer';
 import { updateSliderPosition, getValue } from './time-slider';
-import { dayCount } from '../common/time-utils';
+import { dayCount, ticksPerDay } from '../common/time-utils';
 
 let playPauseButton = null;
 let timerInstance = null;
 let isPlaying = false;
 let sliderValue = 0;
-
-const ONE_SEC = 1000;
-const totalTime = 10 * 10 * ONE_SEC;
-const delay = totalTime / dayCount;
 
 function stop () {
   isPlaying = false;
@@ -21,7 +17,6 @@ function play () {
   if (timerInstance) {
     timerInstance = null;
   }
-
   isPlaying = true;
   playPauseButton.innerHTML = '&#9208;'; // pause
   sliderValue = getValue();
@@ -29,14 +24,14 @@ function play () {
     console.log('play-pause-control:time-elapsed:', elapsed);
     if (!isPlaying) {
       stop();
-    } else if (sliderValue > dayCount) {
+    } else if (sliderValue > (dayCount * ticksPerDay)) {
       sliderValue = 0;
       updateSliderPosition(sliderValue);
     } else {
       sliderValue = sliderValue + 1;
       updateSliderPosition(sliderValue);
     }
-  }, delay);
+  }, 1000); // every second
 }
 
 function onClick (event) {
