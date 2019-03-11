1967f31e18b7361cbd6af12037f26f73332ac970
Taras Novak <taras.novak@randomfractals.com>
2019-03-11 11:50:27 -0500

    time format on hourly scale for date filter and debug

--- src/common/time-utils.js
+++ src/common/time-utils.js
@@ -3,7 +3,7 @@ import { utcFormat } from 'd3-time-format';
 import { startDate, endDate, ticksPerDay } from './config';
 
 // https://github.com/d3/d3-time-format#locale_format
-const timeFormatString = '%Y-%m-%d 00:00:00';
+const timeFormatString = '%Y-%m-%d %H:00:00';
 const dayFormatString = '%B %d, %A, %I %p'; // November 8, Monday, 6 AM
 const MSECS_IN_DAY = 1000 * 60 * 60 * 24;
 
