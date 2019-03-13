c66874a89b8a3ab19460ecbc716df43e3f92fc9f
Taras Novak <taras.novak@randomfractals.com>
2019-03-12 21:11:28 -0500

    bar selction alpha changes

--- src/components/damage-chart.js
+++ src/components/damage-chart.js
@@ -1,23 +1,23 @@
-import {default as vegaEmbed } from 'vega-embed';
-import { getLabel, getColor } from '../common/damage-color-palette';
-import { zoomOut } from '../components/map';
-import { dispatcher } from '../common/dispatcher';
+import { default as vegaEmbed } from "vega-embed";
+import { getLabel, getColor } from "../common/damage-color-palette";
+import { zoomOut } from "../components/map";
+import { dispatcher } from "../common/dispatcher";
 
 let damageChart = null;
 export function initDamageChart() {
-  damageChart = document.querySelector('#damage-chart');
-  let showAllDamageLink = document.querySelector('#show-all-damage-link');
-  showAllDamageLink.addEventListener('click', onShowAllDamageLinkClick);
-};
-
-function onShowAllDamageLinkClick (event) {
-  showDamage('all');
+  damageChart = document.querySelector("#damage-chart");
+  let showAllDamageLink = document.querySelector("#show-all-damage-link");
+  showAllDamageLink.addEventListener("click", onShowAllDamageLinkClick);
 }
 
-function showDamage(damage = 'all') {
-  console.log('damage-chart:showDamage:', damage);
-  dispatcher.call('damageFilter', null, damage); // null = that/this context
-  if (damage === 'all') {
+function onShowAllDamageLinkClick(event) {
+  showDamage("all");
+}
+
+function showDamage(damage = "all") {
+  console.log("damage-chart:showDamage:", damage);
+  dispatcher.call("damageFilter", null, damage); // null = that/this context
+  if (damage === "all") {
     // zoom out for full area damage display
     zoomOut();
   }
@@ -26,67 +26,88 @@ function showDamage(damage = 'all') {
 export function updateDamageChart(damageData) {
   // console.log('damage-chart:updateDamageChart(): damage-data:', damageData);
   const chartData = damageData.map(damage => {
-      return {damage: getLabel(damage.key0), color: getColor(damage.key0), count: damage.val};
-    });
-  
+    return {
+      damage: getLabel(damage.key0),
+      color: getColor(damage.key0),
+      count: damage.val
+    };
+  });
+
   // damage bar chart vega spec
   const vegaSpec = {
-    "width": 180,
-    "height": 120,
-    "padding": 10,
-    "data": {
-      "values": chartData
+    width: 180,
+    height: 120,
+    padding: 10,
+    data: {
+      values: chartData
     },
-    "encoding": {
-      "x": {
-        "field": "count", 
-        "type": "quantitative",
-        "axis": {
-          "title": "",
-          "tickCount": 3,
-          "format": ",d",
-          "grid": false
+    encoding: {
+      x: {
+        field: "count",
+        type: "quantitative",
+        axis: {
+          title: "",
+          tickCount: 3,
+          format: ",d",
+          grid: false
         }
       },
-      "y": {
-        "field": "damage", 
-        "type": "ordinal",
-        "axis": {
-          "title": ""
+      y: {
+        field: "damage",
+        type: "ordinal",
+        axis: {
+          title: ""
         }
       },
-      "color": {
-        "field": "color", 
-        "type": "nominal", 
-        "scale": null
+      color: {
+        field: "color",
+        type: "nominal",
+        scale: null
       },
-      "tooltip": {
-        "field": "count", 
-        "type": "quantitative",
-        "format": ",d"
+      tooltip: {
+        field: "count",
+        type: "quantitative",
+        format: ",d"
       }
     },
-    "layer": [{
-      "mark": "bar"
-    }, {
-      "mark": {
-        "type": "text",
-        "align": "left",
-        "baseline": "middle",
-        "dx": 3
+    layer: [
+      {
+        mark: {
+          type: "bar",
+          stroke: "#eee",
+          cursor: "pointer"  
+        }
       },
-      "encoding": {
-        "text": {
-          "field": "count", 
-          "type": "quantitative",
-          "format": ",d"
+      {
+        mark: {
+          type: "text",
+          align: "left",
+          baseline: "middle",
+          dx: 3
+        },
+        encoding: {
+          text: {
+            field: "count",
+            type: "quantitative",
+            format: ",d"
+          }
         }
       }
-    }]
+    ],
+    selection: {
+      "barSelection": {
+        fields: ["damage"],
+        on: "click",
+        type: "single"
+      }
+    }
   };
 
-  // render vega chart
-  vegaEmbed('#damage-chart', vegaSpec, {mode: 'vega-lite'});
-};
+  // render vega damage chart
+  vegaEmbed("#damage-chart", vegaSpec, { mode: "vega-lite" }).then(result => {
+    // add damage bar selection handler
+    result.view.addSignalListener("barSelection", (name, value) => showDamage(value));
+  });
+}
 
 export default damageChart;
